2e525cfacdfce7a86660b4cdf97c97dedd02e756
Lucas Calje <lucas@calje.eu>
2014-03-23 14:38:59 +0100

    cleanup

--- client/handlebars-helpers.js
+++ client/handlebars-helpers.js
@@ -1,5 +1,6 @@
 Handlebars.registerHelper('isSelected', function(v1, v2, options) {
     debugger;
+    
     //if(v1 == v2) { return options.fn(this); }
     //else { return options.inverse(this); }
 });
