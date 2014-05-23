6a2b04a3e42c44406648aea173ba9c9eb7e7a28d
Lucas Calje <lucas@frontmen.nl>
2014-05-23 14:15:09 +0200

    Refactored + styles home screen

--- client/home.js
+++ client/home.js
@@ -1,9 +1,25 @@
+var error = null;
+
 Template.home.projects = function () {
-    return Projects.find();
+    return Projects.find({}, {sort: {name: 1}});
 };
 
+Template.home.errorMsg = function () {
+  return error;
+}
+
 Template.home.events = {
-    'click [create-project]': function () {
-        Meteor.call('createProject', {name: $('[project-name]').val(), created: new Date().getTime()});
+    'click [submit-project]': function () {
+      var name = $('[project-name]').val(),
+          project = Projects.findOne({name: name});
+
+      if (project) {
+        App.errorMessage = 'Project already exists';
+        $('[error-dialog]').dialog('open');
+      }
+      else {
+        Meteor.call('createProject', {name: name,  created: new Date().getTime()});
+      }
+      $('[project-name]').val('');
     }
 };
