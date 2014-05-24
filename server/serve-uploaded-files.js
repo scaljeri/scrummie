70edf8b5d94b604b72147678e2fecf0f98d5f31c
Lucas Calje <lucas@frontmen.nl>
2014-05-24 22:47:01 +0200

    Fixed serving uploaded images

--- server/serve-uploaded-files.js
+++ server/serve-uploaded-files.js
@@ -1,27 +1,32 @@
-var fs = Meteor.require('fs');
+//var fs = Meteor.require('fs');
+var fs = Npm.require('fs');
 
 
 if (Meteor.isServer) {
-  WebApp.connectHandlers.stack.splice(0, 0, {
-    route: '/uploads',
-    handle: function (req, res, next) {
+  //WebApp.connectHandlers.stack.splice(0, 0, {
+  WebApp.connectHandlers.use(function (req, res, next) {
+    //route: '/uploads',
+    //handle: function (req, res, next) {
 
-      console.log("SERVRE UPLOADED FILES");
-      var path = req.originalUrl.substr(1);
-      console.log(process.env.PWD + '/.' + path);
+    console.log("SERVRE UPLOADED FILES");
+    var path = req.originalUrl.substr(1);
+    console.log(process.env.PWD + '/.' + path);
 
-      fs.readFile(process.env.PWD + '/.' + path, {encoding: 'binary'}, function (err,data) {
-        if (err) {
-          return console.log(err);
-        }
-        res.writeHead(200, {
-          'Content-Type': 'image/png'
-        });
-        //res.setEncoding("binary");
-        res.write(data);
-        res.end();
-        console.log("DNE");
-      });
-    }
+    //fs.readFileSync(process.env.PWD + '/.' + path, {encoding: 'binary'}, function (err,data) {
+    var data = fs.readFileSync(process.env.PWD + '/.' + path, data);
+    //if (err) {
+      //return console.log(err);
+    //}
+    res.writeHead(200, {
+      'Content-Type': 'image/png'
+    });
+    //res.setEncoding("binary");
+    res.write(data);
+    res.end();
+    console.log("DNE");
   });
+  //}
+//}
+//)
+//;
 }
