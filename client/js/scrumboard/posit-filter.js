2d3f6fbce573062d86e09fed7567ffa86a7656ee
Lucas Calje <lucas@frontmen.nl>
2014-07-04 20:38:41 +0200

    Fixed color filter not closing on outside click

--- client/js/scrumboard/posit-filter.js
+++ client/js/scrumboard/posit-filter.js
@@ -1,39 +1,47 @@
 Template.postitFilter.colors = function () {
-  return TaskColors.find({}, {sort: {index: 1}}).fetch();
+    return TaskColors.find({}, {sort: {index: 1}}).fetch();
 };
 
 Template.postitFilter.rendered = function () {
-  var select = $('[postit-filter]');
+    var select = $('[postit-filter]');
 
-  select.select2({
-    allowClear: true,
-    formatResult: format,
-    formatSelection: format,
-    minimumResultsForSearch: -1,
-    placeholder: "All",
-    dropdownCssClass: 'postit-filter__dropdown',
-    containerCssClass: 'select2-colors',
-    width: 150
+    select.select2({
+        allowClear: true,
+        formatResult: format,
+        formatSelection: format,
+        minimumResultsForSearch: -1,
+        placeholder: "All",
+        dropdownCssClass: 'postit-filter__dropdown',
+        containerCssClass: 'select2-colors',
+        width: 150
 
-  });
+    });
 
-  select.on('change', function () {
-    if (select.val() === '') {
-      App.filterColorId = null;
-    }
-    else {
-      App.filterColorId = TaskColorsSetup.findOne({value: select.val()})._id;
-    }
-  });
+    select.on('change', function () {
+        if (select.val() === '') {
+            App.filterColorId = null;
+        }
+        else {
+            App.filterColorId = TaskColorsSetup.findOne({value: select.val()})._id;
+        }
+    })
+        .on('select2-open', function () {
+            App.outsideClick.register('[postit-filter]', hide, true);
+        });
 };
+
+function hide() {
+    $('[postit-filter]').select2('close');
+    App.outsideClick.remove(hide);
+}
 
 /*
-Template.postitFilter.events = {
-};
-*/
+ Template.postitFilter.events = {
+ };
+ */
 
 function format(color) {
-  return ['<span class="select-option-color"',
-      'style="background-color:' + color.id + '"></span>',
-      '<h3 class="select-option-title">' + color.text + '</h3>'].join('');
+    return ['<span class="select-option-color"',
+            'style="background-color:' + color.id + '"></span>',
+            '<h3 class="select-option-title">' + color.text + '</h3>'].join('');
 }
