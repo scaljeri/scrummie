e2e5ced820bc302b25db9012e2fd06e805e0d19a
Lucas Calje <lucas@frontmen.nl>
2014-07-08 23:15:25 +0200

    Implemented Lanes configuration

--- client/js/config/lanes.js
+++ client/js/config/lanes.js
@@ -1,3 +1,18 @@
 Template.configLanes.lanes = function () {
     return LanesSetup.find({}, {sort: {index: 1}});
 };
+
+Template.configLanes.rendered = function () {
+    $(this.find('.sortable'))
+        .sortable()
+        .disableSelection();
+};
+
+Template.configLanes.events = {
+    'click [config-color__trash]': function (e) {
+        debugger;
+    },
+    'click [config-color__edit]': function (e) {
+        debugger;
+    }
+};
